05fc11aa14d957d01f806ec2cbf10f93a4bf6305
Zannatul Ferdousi Hema <shanzidadisha1234.com>
2023-10-21 23:13:40 +0600

    private route error fix

--- src/Page Layout/PrivateRoute.jsx
+++ src/Page Layout/PrivateRoute.jsx
@@ -6,13 +6,9 @@ const PrivateRoute = ({ children }) => {
   const { user, loading } = useContext(AuthContext);
   const location = useLocation();
 
-  if (loading) {
-    return (
-      <p className="loading loading-dots loading-sm flex justify-center p-4"></p>
-    );
-  } else if (user) {
+  if (user) {
     return children;
-  }
+  } 
   return <Navigate state={location.pathname} to="/signin"></Navigate>;
 };
 
